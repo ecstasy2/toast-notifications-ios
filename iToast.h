909b5d2fea413042adbddf7f6e5018ca7ed75029
Paul Kester <paul.kester@newellco.com>
2013-08-01 13:09:04 -0500

    updated license to 2012 Guru Software

--- iToast.h
+++ iToast.h
@@ -4,7 +4,7 @@ iToast.h
 
 MIT LICENSE
 
-Copyright (c) 2011 Travis CI development team
+Copyright (c) 2012 Guru Software
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
