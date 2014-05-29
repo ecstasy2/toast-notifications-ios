1a2bdcf239278115bc2c7074fc867dd80eb44f83
Carmelo Sui <Carmelosui@gmail.com>
2014-05-29 13:37:30 +0800

    change enum define to a typedef so that including header in multiple .m file won't generate duplacted symbols error

--- iToast.h
+++ iToast.h
@@ -34,7 +34,7 @@ typedef enum iToastGravity {
 	iToastGravityCenter
 }iToastGravity;
 
-enum iToastDuration {
+typedef enum iToastDuration {
 	iToastDurationLong = 10000,
 	iToastDurationShort = 1000,
 	iToastDurationNormal = 3000
